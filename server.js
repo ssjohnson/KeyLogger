d68eb2d288fd2f73f76acfa891f48efda051cdf4
unknown <ssjohnson1990@gmail.com>
2018-10-17 01:44:56 -0400

    Working on FTP server.

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const FtpSrv = require('ftp-srv');
-const ftpServer = new FtpSrv('ftp://127.0.0.1:5555', {greeting:"Hello World"});
+const ftpServer = new FtpSrv('ftp://127.0.0.1:5555', { greeting : "Hello World" });
 
-ftpServer.on('login', (data, resolve, reject) => {});
+ftpServer.on('login', ({connection, username, password}, resolve, reject) => {
+    console.log("Login from: " + connection.ip);
+    console.log("Username:" + username);
+    if(username == 'root' && password == 'root'){
+        console.error("resolving");
+        resolve({root: '/Users/Stephen/Desktop/project/keylogger/', cwd : '/ftpserver/root/'});
+    } else {
+        reject("Connection Refused");
+    }
+});
+
+ftpServer.on('client-error', ({connection, context, error}) => {
+    console.error("CLIENT ERROR:");
+    console.error("CONTEXT : " + context);
+    console.error("ERROR: " + error);
+    connection.close();
+});
+
+ftpServer.on('STOR', (error, filename) => {
+    if(error) {
+        console.error("ERROR UPLOADING FILE: " + filename);
+    }
+});
 
 ftpServer.listen()
 .then(() => {});
